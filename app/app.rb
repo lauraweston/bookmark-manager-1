cefaa3b9d7220406ba195826bb226077d75e2095
Laura Weston <laura.weston@outlook.com>
2016-10-19 16:32:00 +0100

    Add heroku database

--- app/app.rb
+++ app/app.rb
@@ -23,6 +23,6 @@ ENV['RACK_ENV'] ||= 'development'
   run! if app_file == $0
 end
 
-DataMapper.setup(:default,"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
